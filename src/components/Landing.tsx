b97e11d3596cae40f90409295b6c2148523e9e7d
AminBromdhane01 <mohamedaminebenromdhane.com>
2023-07-19 10:21:48 +0100

    first commit

---
+++ src/components/Landing.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Landing.css'
+
+const Landing:React.FC = () => {
+  return (
+    <div id='container'>
+     <button className='btn' >Button One</button>
+       <button className='btn'>Button Two</button>
+    </div>
+  )
+}
+
+export default Landing
